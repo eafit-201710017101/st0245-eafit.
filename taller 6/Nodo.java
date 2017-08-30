d2370e6b305c29d5ea8ffa6caaf3cc46e1226eee
eafit-201710017101 <30441160+eafit-201710017101@users.noreply.github.com>
2017-08-29 20:55:51 -0500

    Create Nodo.java

---
+++ taller 6/Nodo.java
@@ -0,0 +1,17 @@
+public class Nodo
+{
+   int data;
+   Nodo siguiente;
+        
+   public Nodo(int data) {
+       this.data = data;
+   }
+   
+   public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+    
+   
+}
+
